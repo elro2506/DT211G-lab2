ce5d07dbc3650537c25bf13f133632350cd50fe2
Elin Ronda <elro2506@student.miun.se>
2026-02-03 11:15:03 +0100

    Utskrift av kurser och sortering är tillagt

--- src/main.js
+++ src/main.js
@@ -1,13 +1,31 @@
 "use strict";
 
+//Sparar kurserna globalt så jag kan sortera dem sen
+let kursSchema = [];
 //När hela HTML-dokumntet laddats så körs loadCourses
-document.addEventListener("DOMContentLoaded", loadCourses);
+document.addEventListener("DOMContentLoaded", () => {
+  loadCourses(); //Hämtar kurser så jag kan skriva ut dem i tabellen
+
+//Händelselyssnare för sortering för kurskoden, kursnamnet och progressionen
+document.querySelector("#sortCode").addEventListener("click", ()=> {
+const sortedByCode = [...kursSchema].sort((a, b) => a.code.localeCompare(b.code));
+fillTable(sortedByCode); //Skickar resultatet till tabellen
+});
+document.querySelector("#sortName").addEventListener("click", ()=> {
+const sortedByName = [...kursSchema].sort((a, b) => a.coursename.localeCompare(b.coursename));
+fillTable(sortedByName);
+});
+document.querySelector("#sortProgression").addEventListener("click", ()=> {
+const sortedByProgression = [...kursSchema].sort((a, b) => a.progression.localeCompare(b.progression));
+fillTable(sortedByProgression);
+});
+});
 
 //Här gör jag en fetch på API/kursdatan och ser till så att det returneras som javascript
 async function fetchCourses() {
   const response = await fetch('https://webbutveckling.miun.se/files/ramschema.json');
   if (!response.ok) { //Kollar så servern svarar ok
-    throw new Error('Kan inte hämta kursdata');
+    throw new Error('Kan inte hämta kursdata'); //Om servern inte svarar ok så skrivs detta ut
     }
     return await response.json(); //Svaret görs om till javascript-objekt
 }
@@ -17,6 +35,9 @@ async function loadCourses() {
   try {
     const data = await fetchCourses(); //Hämtar kurser från dokumentet
     fillTable(data);                    //Datan skickas till tabellen
+  
+    kursSchema = data;  //Sparar kurserna globalt
+
   }
   catch (error) {
     console.error ("Det uppstod ett fel:", error.message);
@@ -25,18 +46,8 @@ async function loadCourses() {
 
 //Funktion för att skapa rader i tabellen och fylla med data
 function fillTable(courses) {
-const table = document.getElementById("courses");
-table.innerHTML = `
-<thead>
-<tr>
-<th>Kurskod</th>
-<th>Namn</th>
-<th>Progression</th>
-</tr>
-</thead>
-<tbody></tbody>`;
-
-const tbody = table.querySelector("tbody")
+const tbody = document.querySelector("#courses tbody");
+tbody.innerHTML =""; //Rensar gamla raderna
 
 //Gör en loop för varje kurs och skapar en rad i tabellen
 courses.forEach(course => {
@@ -46,7 +57,7 @@ tr.innerHTML = `
 <td>${course.coursename}</td>
 <td>${course.progression}</td>
 `;
-tbody.appendChild(tr); //lägger till raden i tabellen så att den syns
+tbody.appendChild(tr); //Lägger till raden i tabellen så att den syns
 });
 }
 
